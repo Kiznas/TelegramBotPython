2d07e57a4e3655bb37a41d5b5d3a3faa5633409e
Kinzas <64547234+Kiznas@users.noreply.github.com>
2023-07-31 01:54:14 +0200

    fs

--- background.py
+++ background.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from threading import Thread
-
+ 
+ 
 app = Flask('')
-
+ 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def home():
+  return "I'm alive"
+ 
 def run():
-  app.run(host='0.0.0.0', port=8080)
-
+  app.run(host='0.0.0.0', port=80)
+ 
 def keep_alive():
   t = Thread(target=run)
   t.start()
